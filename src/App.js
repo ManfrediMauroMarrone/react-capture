edef6ae62932685e0c633f2fb35eb04795cea513
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2021-04-16 15:35:48 +0200

    added style for ContactUs, MovieDetail and OurWork pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from "react";
 import AboutUs from './pages/AboutUs';
-import GlobalStyle from './components/GlobalStyle'
+import GlobalStyle from './components/GlobalStyle';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
 import Nav from './components/Nav';
+import MovieDetail from './pages/MovieDetail';
+//Router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     
     <div className="App">
       <GlobalStyle/>
-      <Nav/>
-      <AboutUs/>
+      <Nav />
+      <Switch>
+      <Route path="/" exact>
+        <AboutUs />
+
+      </Route>
+      <Route path="/work" exact>
+        <OurWork />
+
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail/>
+        </Route>
+      <Route path="/contact">
+        <ContactUs/>
+
+      </Route>
+
+      </Switch>
 
     </div>
   );
